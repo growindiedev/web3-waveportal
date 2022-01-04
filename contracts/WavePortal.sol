d788049d6150780ccc1758c6e2dca338506132a4
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-01-04 06:56:58 +0530

    finished waveportal v1.0

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -29,7 +29,7 @@ contract WavePortal {
   function wave(string memory _message) public {
 
     require(
-      lastWavedAt[msg.sender] + 15 minutes < block.timestamp, "Wait 15 minutes"
+      lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "Wait 30 seconds"
     );
 
     lastWavedAt[msg.sender] = block.timestamp;
